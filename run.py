c68b178fdfc0c22f9c878a7f89efff83f8124c96
Jonathan Silva <33843748+jonathansilva@users.noreply.github.com>
2020-05-15 22:20:12 -0300

    Ajustes

--- run.py
+++ run.py
@@ -2,11 +2,118 @@ import cv2
 import numpy as np
 #import pyscreenshot as ImageGrab
 
+'''
+    Requerimento de resolução dos screenshots: 1080 x 1920
+'''
+
 def passFunction():
     pass
 
+# Remover este retângulo e criar linhas ( 6 ) que vão servir como paredes da mesa
 def drawRectangle(frame):
     return cv2.rectangle(frame, (76, 85), (563, 329), (0, 0, 0), thickness = 1)
+
+def drawLineLeft(frame):
+    width = frame.shape[1] # 640
+    height = frame.shape[0] # 360
+
+    xA = int(width * 0.115625) # width 74
+    yA = int(height * 0.288889) # height 104
+
+    xB = int(width * 0.115625) # width 74
+    yB = int(height * 0.855556) # height 308
+
+    start_point = (xA, yA)
+    end_point = (xB, yB)
+
+    color = (0, 0, 0)
+    thickness = 3
+    image = cv2.line(frame, start_point, end_point, color, thickness)
+
+def drawLineRight(frame):
+    width = frame.shape[1] # 640
+    height = frame.shape[0] # 360
+
+    xA = int(width * 0.882813) # width 565
+    yA = int(height * 0.288889) # height 104
+
+    xB = int(width * 0.882813) # width 565
+    yB = int(height * 0.855556) # height 308
+
+    start_point = (xA, yA)
+    end_point = (xB, yB)
+
+    color = (0, 0, 0)
+    thickness = 3
+    image = cv2.line(frame, start_point, end_point, color, thickness)
+
+def drawLineTopLeft(frame):
+    width = frame.shape[1] # 640
+    height = frame.shape[0] # 360
+
+    xA = int(width * 0.148438) # width 95
+    yA = int(height * 0.230556) # height 83
+
+    xB = int(width * 0.475) # width 304
+    yB = int(height * 0.230556) # height 83
+
+    start_point = (xA, yA)
+    end_point = (xB, yB)
+
+    color = (0, 0, 0)
+    thickness = 3
+    image = cv2.line(frame, start_point, end_point, color, thickness)
+
+def drawLineTopRight(frame):
+    width = frame.shape[1] # 640
+    height = frame.shape[0] # 360
+
+    xA = int(width * 0.523438) # width 335
+    yA = int(height * 0.230556) # height 83
+
+    xB = int(width * 0.848438) # width 543
+    yB = int(height * 0.230556) # height 83
+
+    start_point = (xA, yA)
+    end_point = (xB, yB)
+
+    color = (0, 0, 0)
+    thickness = 3
+    image = cv2.line(frame, start_point, end_point, color, thickness)
+
+def drawLineBottomLeft(frame):
+    width = frame.shape[1] # 640
+    height = frame.shape[0] # 360
+
+    xA = int(width * 0.148438) # width 95
+    yA = int(height * 0.919444) # height 331
+
+    xB = int(width * 0.475) # width 304
+    yB = int(height * 0.919444) # height 331
+
+    start_point = (xA, yA)
+    end_point = (xB, yB)
+
+    color = (0, 0, 0)
+    thickness = 3
+    image = cv2.line(frame, start_point, end_point, color, thickness)
+
+def drawLineBottomRight(frame):
+    width = frame.shape[1] # 640
+    height = frame.shape[0] # 360
+
+    xA = int(width * 0.523438) # width 335
+    yA = int(height * 0.919444) # height 331
+
+    xB = int(width * 0.848438) # width 543
+    yB = int(height * 0.919444) # height 331
+
+    start_point = (xA, yA)
+    end_point = (xB, yB)
+
+    color = (0, 0, 0)
+    thickness = 3
+    image = cv2.line(frame, start_point, end_point, color, thickness)
 
 def draw_lines(frame, lines):
     for rho, theta in lines[0]:
@@ -44,14 +151,14 @@ def main():
     original_height = int(original_frame.shape[0] * scale_percent / 100)
     frame = cv2.resize(original_frame, (original_width, original_height))
 
-    ''' Bônus
-    height = frame.shape[0]
-    width = frame.shape[1]
+    drawLineLeft(frame)
+    drawLineRight(frame)
+    drawLineTopLeft(frame)
+    drawLineTopRight(frame)
+    drawLineBottomLeft(frame)
+    drawLineBottomRight(frame)
 
-    print('Height: ', height) # 360
-    print('Width: ', width) # 640
-    '''
-    drawRectangle(frame)
+    #drawRectangle(frame)
 
     while True:
         _threshold1 = cv2.getTrackbarPos('threshold 1', windowName)
@@ -68,7 +175,7 @@ def main():
 
         original_image_with_hough_lines = weighted_frame(hough_lines_image, frame)
 
-        cv2.imshow(windowName, original_image_with_hough_lines)
+        cv2.imshow(windowName, frame) # original_image_with_hough_lines
         #cv2.imshow('Edges', edges)
 
         key = cv2.waitKey(1)
